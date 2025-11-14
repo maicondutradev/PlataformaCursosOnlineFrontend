21cf2c520d75389ecc70b90a0ef479fae9eb00e4
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2025-11-14 16:57:45 -0300

    Atualizações

--- plataforma-cursos/src/app/curso/curso-list.ts
+++ plataforma-cursos/src/app/curso/curso-list.ts
@@ -1,17 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
-import { MatTableModule } from '@angular/material/table';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { CursoService } from '../services/curso';
 import { Curso } from '../models/curso';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-curso-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule, RouterModule, MatIconModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    RouterModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './curso-list.html',
   styleUrl: './curso-list.css'
 })
@@ -21,12 +34,35 @@ export class CursoList implements OnInit {
 
   cursos: Curso[] = [];
 
+  cursosFiltrados!: Observable<Curso[]>;
+
+  searchControl = new FormControl('');
+
   ngOnInit() {
-    this.carregar();
+    this.carregarCursos();
   }
 
-  carregar() {
-    this.service.listar().subscribe(dados => this.cursos = dados);
+  carregarCursos() {
+    this.service.listar().subscribe(dados => {
+      this.cursos = dados;
+
+      this.cursosFiltrados = this.searchControl.valueChanges.pipe(
+        startWith(''),
+        map(termo => this.filtrarCursos(termo))
+      );
+    });
+  }
+
+  private filtrarCursos(termo: string | null): Curso[] {
+    const termoBusca = (termo || '').toLowerCase();
+
+    if (!termoBusca) {
+      return this.cursos;
+    }
+
+    return this.cursos.filter(curso =>
+      curso.nome.toLowerCase().includes(termoBusca)
+    );
   }
 
   editar(id: number) {
@@ -34,8 +70,10 @@ export class CursoList implements OnInit {
   }
 
   remover(id: number) {
-    if(confirm('Deseja remover este curso?')) {
-      this.service.remover(id).subscribe(() => this.carregar());
+    if (confirm('Deseja remover este curso?')) {
+      this.service.remover(id).subscribe(() => {
+        this.carregarCursos();
+      });
     }
   }
 }
